20ccf5351006fbb1944862996d5bd418f93bc065
Daniela <danielacardona2203@gmail.com>
2023-08-05 23:41:02 +0200

    Create variables

---
+++ script.js
@@ -0,0 +1,6 @@
+let  input = document.querySelector(".input");
+character = document.querySelector('.character');
+word = document.querySelector('.word');
+readingTime = document.querySelector('.reading-time');
+wordLimit = document.querySelector('.word-limit');
+WORD_LIMIT = 10;
