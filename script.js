6ca4f66d96ba0b96827200e69e104d12c576c9d3
Daniela <danielacardona2203@gmail.com>
2023-08-06 19:55:56 +0200

    Implement input event handlers for character and word counting

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-let  input = document.querySelector(".input");
+let input = document.querySelector('.input');
 character = document.querySelector('.character');
 word = document.querySelector('.word');
 readingTime = document.querySelector('.reading-time');
 wordLimit = document.querySelector('.word-limit');
-WORD_LIMIT = 10;
+// WORD_LIMIT = 10;
+
+input.addEventListener('keyup', characterCount);
+input.addEventListener('keyup', wordCounter);
+
+function characterCount() {
+  character.innerHTML = input.value.length;
+}
+
+function wordCounter(e) {
+    let words = input.value.match(/\b[-?(\w+)?]+\b/gi);
+    let wordsLeft = 10;
+
+    word.innerHTML = words.length;
+    wordLimit.innerHTML = wordsLeft - words.length;
+}
