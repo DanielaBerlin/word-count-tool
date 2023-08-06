211dff99f8cf48db8d15b17e848276ac4bc028e3
Daniela <danielacardona2203@gmail.com>
2023-08-06 21:30:20 +0200

    Implement word count limit in input field

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ character = document.querySelector('.character');
 word = document.querySelector('.word');
 readingTime = document.querySelector('.reading-time');
 wordLimit = document.querySelector('.word-limit');
-// WORD_LIMIT = 10;
+WORD_LIMIT = 10;
 
 input.addEventListener('keyup', characterCount);
 input.addEventListener('keyup', wordCounter);
@@ -16,6 +16,24 @@ function wordCounter(e) {
     let words = input.value.match(/\b[-?(\w+)?]+\b/gi);
     let wordsLeft = 10;
 
+    if (words) {
     word.innerHTML = words.length;
     wordLimit.innerHTML = wordsLeft - words.length;
+    } else {
+        word.innerHTML = 0;
+    }
+
+// word Limit block of code
+    input.addEventListener("keydown", function (e) {
+        words = input.value.match(/\b[-?(\w+)?]+\b/gi);
+        if (words) {
+            if (words.length > WORD_LIMIT - 1 && e.code !== "Backspace") {
+                e.preventDefault();
+                console.log("Word limit reached");
+            }
+        }
+    });
+    //Reading time based on 225 words/min
+    
+   
 }
